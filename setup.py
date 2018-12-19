adf7aaf78e5e932d7a5fc95cf254b9acae08242c
Vasco Tenner <vasco@tenner.nl>
2018-12-19 11:33:07 +0100

    Working setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='0.0.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -125,7 +125,8 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['filename_tools']),  # Required
+    packages=['filename_tools'],
+    #find_packages(['filename_tools']),  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
